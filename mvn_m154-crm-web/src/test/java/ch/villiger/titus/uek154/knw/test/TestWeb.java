b9c54f23f3e1c7f438b5767162cc0776a1787636
Titus Villiger <titus.villiger@gibmit.ch>
2019-06-21 14:59:34 +0200

    Changed selenium unit test, still no 7070 found

--- mvn_m154-crm-web/src/test/java/ch/villiger/titus/uek154/knw/test/TestWeb.java
+++ mvn_m154-crm-web/src/test/java/ch/villiger/titus/uek154/knw/test/TestWeb.java
@@ -1,21 +1,23 @@
 package ch.villiger.titus.uek154.knw.test;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 public class TestWeb {
 
 	@Test
-	public void test() {
+	public void test() throws InterruptedException {
 		
 		
 		// TODO: TEST DOES FAIL ("Keine Verbindung zu 7070 aufgebauut" -> Diabled for now
-		
-		// Configure firefox driver
 		/*
+		// Configure firefox driver
+	
 		System.setProperty("webdriver.gecko.driver", "C:\\DevTools\\geckodriver\\geckodriver.exe");
 		
 		// Start firefox driver
@@ -32,19 +34,22 @@ public class TestWeb {
 		// WebDriver driver = new ChromeDriver(options);
 
 		// Start website
-		driver.get("http://localhost:7070");
-		
-		// Use website
-		driver.findElement(By.xpath("//input[@name='q']")).sendKeys("Linux");
-		driver.findElement(By.xpath("//input[@name='q']")).submit();
+		driver.get("http://localhost:7070/");
+		String title = driver.getTitle();
+		assertTrue(title.contains("CRM App"));
 
-		// Wait for 5 Sec
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		WebElement forename = driver.findElement(By.xpath("/html/body/form[1]/input[1]"));
+		forename.sendKeys("Meine Welt");
+
+		WebElement button = driver.findElement(By.xpath("/html/body/form[1]/p/input"));
+		
+		Thread.sleep(5000);
+		
+		button.click();
+
+		WebElement username = driver.findElement(By.xpath("/html/body/span"));
+
+		assertTrue(username.getText().trim().contains("Meine Welt"));
 
 		// Close driver
 		driver.quit();
